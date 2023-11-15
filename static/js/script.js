c10f66bbd77a6a270f56e5eee1fbcbbc51a7a74f
Retesh Kumar Sharma <61469396+rksharma7071@users.noreply.github.com>
2023-11-15 10:38:50 +0530

    update 0.3

--- static/js/script.js
+++ static/js/script.js
@@ -8,6 +8,28 @@ $(document).ready(function () {
     });
 });
 
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
 
 function menu_click() {
 	if (document.getElementById("nav").style.display == "block"){
